7a6eabe4f4e7e968bc87f95b7d8dcb12c637923e
Michele Fattoruso <michele.fattoruso@gmail.com>
2019-02-01 18:17:57 -0600

    the website now sends a confirmation email every time someone rsvp

--- engine/sendMail.js
+++ engine/sendMail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
+const fs = require('fs');
+
 
 var send = async (body) => {
-    console.log(JSON.stringify(body));
+    // console.log(JSON.stringify(body));
 
     var {
         fname,
@@ -38,6 +40,7 @@ var send = async (body) => {
         // html: "<b>Hello world?</b>" // html body
     };
 
+    console.log(`Sending confirmation email to: ${to}`)
     // Returning a promise, so we can handtle the async job
     return new Promise(function (resolve, reject) {
         // Do async job
@@ -61,6 +64,75 @@ var send = async (body) => {
 
 }
 
+var sendConfirmation = async (t,participation) => {
+
+    // var {
+    //     fname,
+    //     lname,
+    //     subject,
+    //     message,
+    //     email
+    // } = body;
+    let account = {
+        user: process.env.MAIL_ADDR,
+        pass: process.env.MAIL_PWD
+    };
+
+    let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_SMTP,
+        port: 465,
+        secure: true,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    });
+
+    var template = fs.readFileSync('./data/mail-template.html',"utf8");
+    // console.log(template);
+
+    template = template.replace(/%TITLE%/, t('Your participation has been confirmed'));
+    template = template.replace(/%INFO%/, participation.numberAdults + t(' Adults and ') + participation.numberChildren + t(' Childrens - Location: ') + t(`${participation.evLocation}`));
+    template = template.replace(/%END%/, t('We\'d love for you to be there.'));
+    // console.log(template);
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+
+        from: `"Michele & Rosa" <${process.env.MAIL_ADDR}>`, // sender address
+        // from: `"${fname} ${lname}" <${email}>`, 
+        to: `${participation.email}`, // list of receivers
+        // to: "juventino990@hotmail.it,info@micheleandrosa.wedding", // list of receivers
+        subject: t('Michele & Rosa Wedding'), // Subject line
+        // text: `${text}`, // plain text body
+        html: template // html body
+    };
+
+    // Returning a promise, so we can handtle the async job
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                resolve(info.response);
+            }
+        });
+    });
+    // // send mail with defined transport object
+    // let info = await transporter.sendMail(mailOptions,function(error, info){
+    //     if (error) {
+    //         // TODO: Handle the case when The mail isn't successfully sent
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
+
+}
+
 module.exports = {
-    send
+    send,
+    sendConfirmation
 };
